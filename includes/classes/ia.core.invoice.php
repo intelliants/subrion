907e1fbfa27846e0a7e518d39b572c77f1b85054
Daiyrbek Artelov <dartelov@intelliants.com>
2016-03-25 16:00:10 +0600

    Resolves #142

--- includes/classes/ia.core.invoice.php
+++ includes/classes/ia.core.invoice.php
@@ -116,7 +116,7 @@ class iaInvoice extends abstractCore
 			. 'FROM `:prefix:table_transactions` t '
 			. 'LEFT JOIN `:prefix:table_invoices` i ON (i.`transaction_id` = t.`id`) '
 			. 'WHERE t.`member_id` = :member AND i.`address1` != "" '
-			. 'ORDER BY t.`date` DESC '
+			. 'ORDER BY t.`date_created` DESC '
 			. 'LIMIT 1';
 		$sql = iaDb::printf($sql, array(
 			'prefix' => $this->iaDb->prefix,
