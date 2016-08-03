95ac42d686624e104be644b517f6ab049aad6623
Vasily Bezruchkin <vbezruchkin@intelliants.com>
2016-08-03 15:26:17 +0600

    Codemirror added.

--- js/ckeditor/config.js
+++ js/ckeditor/config.js
@@ -9,7 +9,7 @@ CKEDITOR.editorConfig = function(config)
 	config.skin = intelli.config.ckeditor_skin || 'bootstrapck';
 	config.filebrowserImageUploadUrl = intelli.config.ia_url + 'actions/?action=ckeditor_upload&Type=Image';
 	config.allowedContent = true;
-	config.extraPlugins = 'embed,autoembed';
+	config.extraPlugins = 'embed,autoembed,codemirror';
 	config.extraAllowedContent = 'a[rel]';
 	CKEDITOR.dtd.$removeEmpty['span'] = false;
 
@@ -19,7 +19,7 @@ CKEDITOR.editorConfig = function(config)
 	}
 
 	config.toolbar_extended = [
-		['Source', '-', 'Maximize'],
+		['Source', 'ShowBlocks', '-', 'Maximize'],
 		['Cut', 'Copy', 'Paste','PasteText','PasteFromWord','-','Undo','Redo'],
 		['Link','Unlink'],
 		['Image','MediaEmbed','CodeSnippet','Table','HorizontalRule','SpecialChar'],
@@ -33,7 +33,7 @@ CKEDITOR.editorConfig = function(config)
 	];
 
 	config.toolbar_dashboard = [
-		['Source', '-', 'Maximize'],
+		['Source', 'ShowBlocks', '-', 'Maximize'],
 		['Cut', 'Copy', 'Paste','PasteText','PasteFromWord','-','Undo','Redo'],
 		['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
 		['NumberedList','BulletedList'],
@@ -66,4 +66,79 @@ CKEDITOR.editorConfig = function(config)
 		config.filebrowserImageBrowseUrl = intelli.config.admin_url + '/uploads/?mode=image';
 		config.filebrowserFlashBrowseUrl = intelli.config.admin_url + '/uploads/?mode=flash';
 	}
+
+	config.codemirror = {
+
+		// Whether or not you want Brackets to automatically close themselves
+		autoCloseBrackets: true,
+
+		// Whether or not you want tags to automatically close themselves
+		autoCloseTags: true,
+
+		// Whether or not to automatically format code should be done when the editor is loaded
+		autoFormatOnStart: true,
+
+		// Whether or not to automatically format code which has just been uncommented
+		autoFormatOnUncomment: true,
+
+		// Whether or not to continue a comment when you press Enter inside a comment block
+		continueComments: true,
+
+		// Whether or not you wish to enable code folding (requires 'lineNumbers' to be set to 'true')
+		enableCodeFolding: true,
+
+		// Whether or not to enable code formatting
+		enableCodeFormatting: true,
+
+		// Whether or not to enable search tools, CTRL+F (Find), CTRL+SHIFT+F (Replace), CTRL+SHIFT+R (Replace All), CTRL+G (Find Next), CTRL+SHIFT+G (Find Previous)
+		enableSearchTools: true,
+
+		// Whether or not to highlight all matches of current word/selection
+		highlightMatches: true,
+
+		// Whether, when indenting, the first N*tabSize spaces should be replaced by N tabs
+		indentWithTabs: false,
+
+		// Whether or not you want to show line numbers
+		lineNumbers: true,
+
+		// Whether or not you want to use line wrapping
+		lineWrapping: true,
+
+		// Define the language specific mode 'htmlmixed' for html  including (css, xml, javascript), 'application/x-httpd-php' for php mode including html, or 'text/javascript' for using java script only
+		mode: 'htmlmixed',
+
+		// Whether or not you want to highlight matching braces
+		matchBrackets: true,
+
+		// Whether or not you want to highlight matching tags
+		matchTags: true,
+
+		// Whether or not to show the showAutoCompleteButton   button on the toolbar
+		showAutoCompleteButton: true,
+
+		// Whether or not to show the comment button on the toolbar
+		showCommentButton: true,
+
+		// Whether or not to show the format button on the toolbar
+		showFormatButton: true,
+
+		// Whether or not to show the search Code button on the toolbar
+		showSearchButton: true,
+
+		// Whether or not to show Trailing Spaces
+		showTrailingSpace: true,
+
+		// Whether or not to show the uncomment button on the toolbar
+		showUncommentButton: true,
+
+		// Whether or not to highlight the currently active line
+		styleActiveLine: true,
+
+		// Set this to the theme you wish to use (codemirror themes)
+		theme: 'default',
+
+		// "Whether or not to use Beautify for auto formatting
+		useBeautify: false
+	};
 };
