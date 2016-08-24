26ce6d03e4e32b6dbd10425d7d028f8db942df49
Batyr Mackenov <jjangaraev@intelliants.com>
2016-08-24 10:07:44 +0600

    Version changed to 4.0.5.10

--- install/modules/module.install.php
+++ install/modules/module.install.php
@@ -24,7 +24,7 @@
  *
  ******************************************************************************/
 
-define('IA_VER', '405');
+define('IA_VER', '40510');
 
 $iaOutput->layout()->title = 'Installation Wizard';
 
