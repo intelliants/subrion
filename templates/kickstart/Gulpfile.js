5a30bcc9a73aa1ef76e26ea2dca1f81da91e3848
Gleb Surinov <gsurinov@intelliants.com>
2017-01-12 15:38:09 +0600

    Kickstart improvements

--- templates/kickstart/Gulpfile.js
+++ templates/kickstart/Gulpfile.js
@@ -1,11 +1,13 @@
-var pjson      = require('./package.json'),
-    gulp       = require("gulp"),
-    gutil      = require('gulp-util'),
-    concat     = require("gulp-concat"),
-    imagemin   = require("gulp-imagemin"),
-    less       = require("gulp-less"),
-    sourcemaps = require('gulp-sourcemaps'),
-    cleanCSS   = require('gulp-clean-css');
+var fs          = require("fs"),
+    serverConf  = (fs.existsSync('./config.json')) ? require('./config.json').server : null,
+    gulp        = require("gulp"),
+    gutil       = require('gulp-util'),
+    concat      = require("gulp-concat"),
+    imagemin    = require("gulp-imagemin"),
+    less        = require("gulp-less"),
+    sourcemaps  = require('gulp-sourcemaps'),
+    cleanCSS    = require('gulp-clean-css'),
+    browserSync = require('browser-sync').create();
 
 var config = {
   paths: {
@@ -41,7 +43,8 @@ gulp.task("less-dev", function(){
         this.emit('end');
     }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.paths.less.dest));
+    .pipe(gulp.dest(config.paths.less.dest))
+    .pipe(browserSync.stream());
 });
 
 gulp.task("less", function() {
@@ -56,13 +59,21 @@ gulp.task("less", function() {
     .pipe(gulp.dest(config.paths.less.dest));
 });
 
+gulp.task('browser-sync', function() {
+  if (serverConf !== null) {
+    browserSync.init(serverConf);
+  } else {
+    console.log('BrowserSync config not specified');
+  }
+});
+
 gulp.task("build", ["less", "images"]);
 
 gulp.task("watch", function() {
   gulp.watch(config.paths.less.path, ["less-dev"]);
 });
 
-gulp.task("dev", ["less-dev", "images", "watch"]);
+gulp.task("dev", ["less-dev", "images", "browser-sync", "watch"]);
 
 gulp.task("default", function() {
   console.log('Silence is gold');
