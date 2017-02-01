02040dfa0151cfe10ce362dc012fad0362f25c64
Vasily Bezruchkin <vbezruchkin@intelliants.com>
2017-02-01 16:58:40 +0600

    Closes #245

--- js/admin/templates.js
+++ js/admin/templates.js
@@ -67,4 +67,25 @@ Ext.onReady(function()
 			}
 		});
 	});
+
+	$('.js-reinstall').click(function(e) {
+		e.preventDefault();
+
+		var $this = $(this);
+		Ext.Msg.show({
+			title: _t('confirm'),
+			msg: _t('are_you_sure_reinstall_template'),
+			buttons: Ext.Msg.YESNO,
+			icon: Ext.Msg.QUESTION,
+			fn: function (btn) {
+				if ('yes' == btn) {
+					// fake input emulation for simplicity
+					var input = $("<input>").attr("type", "hidden").attr("name", 'reinstall').val("reinstall"),
+						form = $this.closest('form');
+					form.append($(input));
+					form.submit();
+				}
+			}
+		});
+	});
 });
