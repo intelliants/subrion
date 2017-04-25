dda3557f1f4f228c9ae733d5c6239c222dcb11b5
Janur Jangaraev <jjangaraev@intelliants.com>
2017-04-25 13:59:05 +0600

    Version changed to 4.1.3.12

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
  *
  ******************************************************************************/
 
-define('IA_VERSION', '4.1.3.11');
+define('IA_VERSION', '4.1.3.12');
 
 if (defined('IA_INSTALL')) {
     return IA_VERSION;
