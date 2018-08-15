da492d70d7918e96c00c33909595bea2a419bbae
Vladimir Semchishin <semchishin.vladimir@gmail.com>
2018-08-15 11:17:52 +0600

    Fixed.

--- modules/blog/manage.php
+++ modules/blog/manage.php
@@ -1,11 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: semch
- * Date: 26.07.2018
- * Time: 10:16
- */
-
+/******************************************************************************
+ *
+ * Subrion - open source content management system
+ * Copyright (C) 2018 Intelliants, LLC <https://intelliants.com>
+ *
+ * This file is part of Subrion.
+ *
+ * Subrion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Subrion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Subrion. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @link https://subrion.org/
+ *
+ ******************************************************************************/
 
 $iaBlog = $iaCore->factoryModule('blog', IA_CURRENT_MODULE);
 $baseUrl = $iaCore->factory('page', iaCore::FRONT)->getUrlByName('blog');
