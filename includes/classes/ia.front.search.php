353182cfae4705cab4b80f77b9fabbf51966e855
alekos <alekos@mac.local>
2019-04-24 14:45:19 +0600

    #793 removed the possibility of SQL injection

--- includes/classes/ia.front.search.php
+++ includes/classes/ia.front.search.php
@@ -107,8 +107,8 @@ class iaSearch extends abstractCore
     {
         $page = isset($params[self::GET_PARAM_PAGE]) ? max((int)$params[self::GET_PARAM_PAGE], 1) : 1;
         $sorting = [
-            isset($params[self::GET_PARAM_SORTING_FIELD]) ? $params[self::GET_PARAM_SORTING_FIELD] : null,
-            isset($params[self::GET_PARAM_SORTING_ORDER]) ? $params[self::GET_PARAM_SORTING_ORDER] : null
+            isset($params[self::GET_PARAM_SORTING_FIELD]) ? mysqli_real_escape_string($params[self::GET_PARAM_SORTING_FIELD]) : null,
+            isset($params[self::GET_PARAM_SORTING_ORDER]) ? mysqli_real_escape_string($params[self::GET_PARAM_SORTING_ORDER]) : null
         ];
 
         $result = [
