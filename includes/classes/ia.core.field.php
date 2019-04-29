7dcdf1ea4649786db5dd5b3e2114e7c24b113830
alekos <alekos@mac.local>
2019-04-29 17:03:23 +0600

    fixed test field id

--- includes/classes/ia.core.field.php
+++ includes/classes/ia.core.field.php
@@ -1317,6 +1317,11 @@ SQL;
     {
         $fieldName = iaSanitize::paranoid($fieldName);
         $itemName = iaSanitize::paranoid($itemName);
+
+        if(!is_int($itemId)){
+            return false;
+        }
+
         $itemId = iaSanitize::paranoid($itemId);
         $tableName = $this->iaCore->factory('item')->getItemTable($itemName);
 
